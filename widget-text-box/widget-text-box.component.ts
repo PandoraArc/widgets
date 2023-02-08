66471671043d9729b3efdff0d756f22a215a256c
PandoraArc <parinthon.nea@gmail.com>
2023-02-08 10:22:40 +0700

    fix widget text box

--- widget-text-box/widget-text-box.component.ts
+++ widget-text-box/widget-text-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-widget-text-box',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./widget-text-box.component.scss']
 })
 export class WidgetTextBoxComponent {
-
+  
+  @Input() placeholder: string = ""
 }
