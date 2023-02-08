0d8f2d742055fa67376708700459f6c4cac0ca7d
PandoraArc <parinthon.nea@gmail.com>
2023-02-08 09:45:19 +0700

    generate widget action bar

--- widget-action-bar/widget-action-bar.component.ts
+++ widget-action-bar/widget-action-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-widget-action-bar',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class WidgetActionBarComponent {
 
+  counter: number = 0;
+  @Input() step: number = 1;
+  
+  decrease() {
+    if (this.counter-this.step >= 0){
+      this.counter = this.counter-this.step;
+    }
+  }
+
+  increase() {
+    if (this.counter+this.step <= 100){
+      this.counter = this.counter+this.step;
+    }
+  }
 }
