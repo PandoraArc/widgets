78867e7963251da3088f2da857c824a798d91924
PandoraArc <parinthon.nea@gmail.com>
2023-02-08 09:56:40 +0700

    add widget text box

---
+++ widget-text-box/widget-text-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-widget-text-box',
+  templateUrl: './widget-text-box.component.html',
+  styleUrls: ['./widget-text-box.component.scss']
+})
+export class WidgetTextBoxComponent {
+
+}
